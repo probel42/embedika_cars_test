da781bd3436a105a394eb741d04a775cdc90444c
probel42 <bel3i@yandex.ru>
2022-07-26 11:43:19 +0500

    backend init

---
+++ backend/src/main/java/ru/ibelan/embedika_cars_test_backend/EmbedikaCarsTestBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.ibelan.embedika_cars_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbedikaCarsTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbedikaCarsTestBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/ibelan/embedika_cars_test_backend/EmbedikaCarsTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibelan.embedika_cars_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedikaCarsTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
