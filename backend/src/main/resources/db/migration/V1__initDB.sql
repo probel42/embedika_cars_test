d0841951b4fe857e8397a9e432ce483bdb9f3c54
probel42 <bel3i@yandex.ru>
2022-07-27 14:26:25 +0500

    init migration fix

--- backend/src/main/resources/db/migration/V1__initDB.sql
+++ backend/src/main/resources/db/migration/V1__initDB.sql
@@ -3,11 +3,10 @@
 CREATE TABLE public.car (
                             id uuid NOT NULL,
                             reg_plate varchar(10) NOT NULL,
-                            model_id uuid NOT NULL,
+                            car_model_id uuid NOT NULL,
                             color_code smallint NOT NULL,
                             year smallint NOT NULL,
                             create_time timestamp NOT NULL,
-                            car_model_id uuid,
                             CONSTRAINT car_pk PRIMARY KEY (id)
 
 );
@@ -17,8 +16,6 @@ COMMENT ON TABLE public.car IS E'Автомобиль';
 COMMENT ON COLUMN public.car.id IS E'Идентификатор';
 -- ddl-end --
 COMMENT ON COLUMN public.car.reg_plate IS E'Номер';
--- ddl-end --
-COMMENT ON COLUMN public.car.model_id IS E'Идентификатор модели';
 -- ddl-end --
 COMMENT ON COLUMN public.car.color_code IS E'Код цвета из справочника';
 -- ddl-end --
@@ -33,9 +30,8 @@ ALTER TABLE public.car OWNER TO embedika_app;
 -- DROP TABLE IF EXISTS public.car_model CASCADE;
 CREATE TABLE public.car_model (
                                   id uuid NOT NULL,
-                                  brand_id uuid NOT NULL,
                                   name varchar(100) NOT NULL,
-                                  car_brand_id uuid,
+                                  car_brand_id uuid NOT NULL,
                                   CONSTRAINT car_model_pk PRIMARY KEY (id)
 
 );
@@ -44,18 +40,9 @@ COMMENT ON TABLE public.car_model IS E'Модель автомобиля';
 -- ddl-end --
 COMMENT ON COLUMN public.car_model.id IS E'Идентификатор';
 -- ddl-end --
-COMMENT ON COLUMN public.car_model.brand_id IS E'Идентификатор бренда';
--- ddl-end --
 COMMENT ON COLUMN public.car_model.name IS E'Название модели';
 -- ddl-end --
 ALTER TABLE public.car_model OWNER TO embedika_app;
--- ddl-end --
-
--- object: car_model_fk | type: CONSTRAINT --
--- ALTER TABLE public.car DROP CONSTRAINT IF EXISTS car_model_fk CASCADE;
-ALTER TABLE public.car ADD CONSTRAINT car_model_fk FOREIGN KEY (car_model_id)
-    REFERENCES public.car_model (id) MATCH FULL
-    ON DELETE CASCADE ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: public.car_brand | type: TABLE --
@@ -76,11 +63,18 @@ COMMENT ON COLUMN public.car_brand.name IS E'Название';
 ALTER TABLE public.car_brand OWNER TO embedika_app;
 -- ddl-end --
 
+-- object: car_model_fk | type: CONSTRAINT --
+-- ALTER TABLE public.car DROP CONSTRAINT IF EXISTS car_model_fk CASCADE;
+ALTER TABLE public.car ADD CONSTRAINT car_model_fk FOREIGN KEY (car_model_id)
+    REFERENCES public.car_model (id) MATCH FULL
+    ON DELETE RESTRICT ON UPDATE RESTRICT;
+-- ddl-end --
+
 -- object: car_brand_fk | type: CONSTRAINT --
 -- ALTER TABLE public.car_model DROP CONSTRAINT IF EXISTS car_brand_fk CASCADE;
 ALTER TABLE public.car_model ADD CONSTRAINT car_brand_fk FOREIGN KEY (car_brand_id)
     REFERENCES public.car_brand (id) MATCH FULL
-    ON DELETE CASCADE ON UPDATE CASCADE;
+    ON DELETE RESTRICT ON UPDATE RESTRICT;
 -- ddl-end --
 
 
